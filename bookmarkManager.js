2dba8e7cca390ca8b2ee2fdd6749c4ba360844cb
ALRW <awerner1@googlemail.com>
2016-02-24 11:21:24 +0000

    completed refactor of methods and test to implement promises

--- bookmarkManager.js
+++ bookmarkManager.js
@@ -1,110 +1,128 @@
-function BookmarkManager() {
-}
+function BookmarkManager() {}
 
+var otherBookmarksFolderId = '2';
 var dayInMilliseconds = 86400000;
 
 BookmarkManager.prototype = {
 
-  initialise: function() {
-    BookmarkManager.prototype.isShortLifeFolder().then(function(result){
-      if(result){
-        BookmarkManager.prototype.setParentId();
-      } else {
-        BookmarkManager.prototype.createShortLifeFolder();
+    initialise: function() {
+        BookmarkManager.prototype.isShortLifeFolder().then(function(result) {
+            if (result) {
+                BookmarkManager.prototype.setParentId().then(function(result) {
+                    if (result === "Success") {
+                        BookmarkManager.prototype.getBookmarks().then(function(result) {
+                            BookmarkManager.prototype.removeBookmarks(result).then(function(bookmarks) {
+                                BookmarkManager.prototype.showBookmarks();
+                            });
+                        });
+                    }
+                });
+            } else {
+                BookmarkManager.prototype.createShortLifeFolder().then(function(result) {
+                    if (result === "Success") {
+                        console.log('Short Life Folder Created');
+                    }
+                });
+            }
+        });
+    },
+
+    isShortLifeFolder: function() {
+        return new Promise(function(resolve) {
+            chrome.bookmarks.search(BookmarkManager.prototype.shortLifeFolder, function(result) {
+                if (result[0]) {
+                    resolve(true);
+                } else {
+                    resolve(false);
+                }
+            });
+        });
+    },
+
+    createShortLifeFolder: function() {
+        return new Promise(function(resolve) {
+            chrome.bookmarks.create(BookmarkManager.prototype.shortLifeFolder, function(response) {
+                BookmarkManager.prototype.newBookmark.parentId = response.id;
+                resolve("Success");
+            });
+        });
+    },
+
+    setParentId: function() {
+        return new Promise(function(resolve) {
+            chrome.bookmarks.getChildren(otherBookmarksFolderId, function(result) {
+                result.forEach(function(node) {
+                    if (node.title === 'Short Life Bookmarks') {
+                        BookmarkManager.prototype.newBookmark.parentId = node.id;
+                        resolve("Success");
+                    }
+                });
+            });
+        });
+    },
+
+    getBookmarks: function() {
+        return new Promise(function(resolve) {
+            var parentId = BookmarkManager.prototype.newBookmark.parentId.toString();
+            chrome.bookmarks.getChildren(parentId, function(bookmarks) {
+                if (bookmarks) {
+                    BookmarkManager.prototype.bookmarks = bookmarks;
+                    resolve(bookmarks);
+                }
+            });
+        });
+    },
+    
+    removeBookmarks: function(bookmarks) {
+        return new Promise(function(response) {
+            bookmarks.forEach(function(bookmark) {
+                if (Date.now() - bookmark.dateAdded > dayInMilliseconds) {
+                    console.log("removing" + bookmark.title);
+                    chrome.bookmarks.remove(bookmark.id);
+                }
+            });
+            BookmarkManager.prototype.getBookmarks().then(function(bookmarks) {
+                response(bookmarks);
+            });
+        });
+    },
+    
+    showBookmarks: function() {
+        var list = $('ul.list-group');
+        for (var i = 0; i < Manager.bookmarks.length; i++) {
+            $item = $('<a href="' + Manager.bookmarks[i].url + '" class="list-group-item">' + Manager.bookmarks[i].title + '</a>');
+            list.append($item);
         }
-      });
-  },
-  createShortLifeFolder: function() {
-    return new Promise(function(resolve){
-      chrome.bookmarks.create(BookmarkManager.prototype.shortLifeFolder, function(response) {
-        BookmarkManager.prototype.newBookmark.parentId = response.id;
-        resolve("Success");
-      });
-    });
-  },
+    },
 
-  isShortLifeFolder: function() {
-    return new Promise(function(resolve){
-      chrome.bookmarks.search(BookmarkManager.prototype.shortLifeFolder, function(result) {
-        if (result[0]) {
-          resolve(true);
-        } else {
-          resolve(false);
+    createBookmark: function() {
+        chrome.tabs.query({
+            currentWindow: true,
+            active: true
+        }, function(tabArray) {
+            BookmarkManager.prototype.newBookmark.title = tabArray[0].title;
+            BookmarkManager.prototype.newBookmark.url = tabArray[0].url;
+            chrome.bookmarks.create(BookmarkManager.prototype.newBookmark);
+        });
+    },
+
+    openBookmark: function(element) {
+        if (element.target.tagName !== 'A') {
+            return;
         }
-      });
-    });
-  },
+        chrome.tabs.create({
+            url: element.target.href
+        });
+    },
 
-  setParentId: function(){
-    return new Promise(function(resolve){
-    chrome.bookmarks.getChildren('2', function(result){
-      result.forEach(function(node){
-        if(node.title === 'Short Life Bookmarks'){
-          BookmarkManager.prototype.newBookmark.parentId = node.id;
-          resolve("Success");
-        }
-      });
-    });
-    });
-  },
-
-  createBookmark: function(){
-    chrome.tabs.query({currentWindow: true, active : true}, function(tabArray){
-      BookmarkManager.prototype.newBookmark.title = tabArray[0].title;
-      BookmarkManager.prototype.newBookmark.url = tabArray[0].url;
-      chrome.bookmarks.create(BookmarkManager.prototype.newBookmark);
-    });
-  },
-
-  getBookmarks: function(){
-    var parentId = BookmarkManager.prototype.newBookmark.parentId.toString();
-    chrome.bookmarks.getChildren(parentId, function(bookmarks){
-      console.log(bookmarks);
-      BookmarkManager.prototype.bookmarks = bookmarks;
-    });
-  },
-
-  showBookmarks: function(){
-    var list = $('ul.list-group');
-    for (var i = 0; i < Manager.bookmarks.length; i++){
-      $item = $('<a href="' + Manager.bookmarks[i].url + '" class="list-group-item">' + Manager.bookmarks[i].title + '</a>');
-      list.append($item);
-    }
-  },
-
-  openBookmark: function(element){
-    if (element.target.tagName !== 'A') {
-      return;
-    }
-    chrome.tabs.create({
-      url: element.target.href
-    });
-  },
-
-  removeBookmarks: function(){
-    new Promise(function(resolve, reject){
-      var parentID = BookmarkManager.prototype.newBookmark.parentId.toString();
-      chrome.bookmarks.getChildren(parentID, function(shortLifeFolderBookmarks){
-        resolve(shortLifeFolderBookmarks);
-      });
-  }).then(function(shortLifeFolderBookmarks){
-    console.log(shortLifeFolderBookmarks);
-    shortLifeFolderBookmarks.forEach(function(bookmark){
-      if(Date.now() - bookmark.dateAdded > dayInMilliseconds){
-        console.log("removing" + bookmark.title);
-        chrome.bookmarks.remove(bookmark.id);
-      }
-    });
-  });
-}
 };
 
 BookmarkManager.prototype.newBookmark = {
-  parentId: '',
-  title: 'default',
-  url: 'http://default.com'
+    parentId: '',
+    title: 'default',
+    url: 'http://default.com'
 };
 
-BookmarkManager.prototype.shortLifeFolder = {"title": "Short Life Bookmarks"};
-
-// BookmarkManager.prototype.bookmarks = [];
+BookmarkManager.prototype.shortLifeFolder = {
+    "title": "Short Life Bookmarks"
+};
--- spec/bookmarkManagerSpec.js
+++ spec/bookmarkManagerSpec.js
@@ -1,80 +1,94 @@
 describe('BookmarkManager', function() {
-var bmm = new BookmarkManager();
-var shortLifeFolder = {"title": "Short Life Bookmarks"};
-var newBm = { parentId: '385', title: undefined, url: undefined };
-var element = {
-  target: {
-    tagName: "A"
-  }
-};
-
-  beforeEach(function() {
-    chrome = {
-      bookmarks: {
-        create: function() {},
-        search: function() {},
-        getChildren: function(){}
-      },
-      tabs: {
-        query: function(){},
-        create: function(){}
-      }
+    var bmm = new BookmarkManager();
+    var shortLifeFolder = {
+        "title": "Short Life Bookmarks"
     };
-    spyOn(chrome.bookmarks, 'create');
-    spyOn(chrome.bookmarks, 'search');
-    spyOn(chrome.bookmarks, 'getChildren').and.callFake(function(){
-      BookmarkManager.prototype.bookmarks = {1:2};
-    });
-    spyOn(chrome.tabs, 'query');
+    var newBm = {
+        parentId: '385',
+        title: undefined,
+        url: undefined
+    };
+    var element = {
+        target: {
+            tagName: "A"
+        }
+    };
 
-  });
+    beforeEach(function() {
+        chrome = {
+            bookmarks: {
+                create: function() {},
+                search: function() {},
+                getChildren: function() {}
+            },
+            tabs: {
+                query: function() {},
+                create: function() {}
+            }
+        };
+        spyOn(chrome.bookmarks, 'create');
+        spyOn(chrome.bookmarks, 'search');
+        spyOn(chrome.bookmarks, 'getChildren').and.callFake(function() {
+            BookmarkManager.prototype.bookmarks = {
+                1: 2
+            };
+        });
+        spyOn(chrome.tabs, 'query');
 
-  describe('#isShortLifeFolder', function() {
-    it('checks whether there is a short-life folder', function() {
-      bmm.isShortLifeFolder().then(function(result){
-        console.log(result);
-      });
-      expect(chrome.bookmarks.search).toHaveBeenCalled();
-      done();
-    });
-  });
-
-  describe('#createShortLifeFolder', function() {
-    it('creates a Short life Bookmark folder', function() {
-      bmm.createShortLifeFolder();
-      expect(chrome.bookmarks.create).toHaveBeenCalledWith(shortLifeFolder, jasmine.any(Function));
-    });
-  });
-
-  describe('#createBookmark', function(){
-    it('creates a new short-life bookmark', function(){
-      bmm.createBookmark();
-      expect(chrome.tabs.query).toHaveBeenCalledWith({currentWindow: true, active : true}, jasmine.any(Function));
-    });
-  });
-
-  describe('#setParentId', function(){
-    it('ensures that new bookmarks are added to the correct folder', function(){
-      bmm.setParentId();
-      expect(chrome.bookmarks.getChildren).toHaveBeenCalledWith('2', jasmine.any(Function));
-    });
-  });
-
-  describe('#getBookmarks', function(){
-    it('gets all the bookmarks in the shortLifeFolder', function(){
-      bmm.getBookmarks();
-      expect(bmm.bookmarks).toEqual({1:2});
-    });
-  });
-
-  describe('#openBookmark', function(){
-    it('exits if the item is not a link', function(){
-      spyOn(chrome.tabs, 'create');
-      element.target.tagName = "B";
-      bmm.openBookmark(element);
-      expect(chrome.tabs.create).not.toHaveBeenCalled();
     });
 
-  });
+    describe('#isShortLifeFolder', function() {
+        it('checks whether there is a short-life folder', function(done) {
+            bmm.isShortLifeFolder();
+            expect(chrome.bookmarks.search).toHaveBeenCalledWith(shortLifeFolder, jasmine.any(Function));
+            done();
+        });
+    });
+
+    describe('#createShortLifeFolder', function() {
+        it('creates a Short life Bookmark folder', function(done) {
+            bmm.createShortLifeFolder();
+            expect(chrome.bookmarks.create).toHaveBeenCalledWith(shortLifeFolder, jasmine.any(Function));
+            done();
+        });
+    });
+
+    describe('#createBookmark', function() {
+        it('creates a new short-life bookmark', function() {
+            bmm.createBookmark();
+            expect(chrome.tabs.query).toHaveBeenCalledWith({
+                currentWindow: true,
+                active: true
+            }, jasmine.any(Function));
+        });
+    });
+
+    describe('#setParentId', function() {
+        it('ensures that new bookmarks are added to the correct folder', function(done) {
+            bmm.setParentId();
+            expect(chrome.bookmarks.getChildren).toHaveBeenCalledWith('2', jasmine.any(Function));
+            done();
+        });
+    });
+
+    describe('#getBookmarks', function() {
+        it('gets all the bookmarks in the shortLifeFolder', function(done) {
+            bmm.getBookmarks();
+            expect(bmm.bookmarks).toEqual({
+                1: 2
+            });
+            done();
+        });
+    });
+
+    describe('#openBookmark', function() {
+        it('exits if the item is not a link', function() {
+            spyOn(chrome.tabs, 'create');
+            element.target.tagName = "B";
+            bmm.openBookmark(element);
+            expect(chrome.tabs.create).not.toHaveBeenCalled();
+        });
+
+    });
 
 });
